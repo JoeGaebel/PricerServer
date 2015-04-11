de321e090d264c79a299db3dbd093952ebc6d531
Joe Gaebel <jgaebel@uwo.ca>
2015-04-11 19:29:08 -0400

    made topic and probability injector

--- server.js
+++ server.js
@@ -18,6 +18,9 @@ MongoClient.connect('mongodb://priceradmin:bluecakes1@ds061611.mongolab.com:6161
     _db = db //this is our global database object
 })
 
+
+
+
 app.use(bodyParser.json()) // get information from html forms
 app.use(bodyParser.urlencoded({
     extended: true
