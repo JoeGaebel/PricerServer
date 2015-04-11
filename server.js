ad6f0ee599017a833db07c2970551f63f0d81b19
Joe Gaebel <jgaebel@uwo.ca>
2015-04-11 16:49:39 -0400

    mongo lab

--- server.js
+++ server.js
@@ -4,7 +4,7 @@
 // get all the tools we need
 var express = require('express')
 var app = express()
-var port = process.env.PORT || 8080
+var port = process.env.PORT || 80
 var bodyParser = require('body-parser')
 var methodOverride = require('method-override')
 var http = require('http')
@@ -12,7 +12,7 @@ var MongoClient = require('mongodb').MongoClient;
 
 // DATBASE CRUMS ======================================
 
-MongoClient.connect('mongodb://usc_admin:admin1@ds031701.mongolab.com:31701/usc_web', function(err, db) {
+MongoClient.connect('mongodb://priceradmin:bluecakes1@ds061611.mongolab.com:61611/pricer', function(err, db) {
     if (err) throw err;
     console.log("Connected to Database");
     _db = db //this is our global database object
